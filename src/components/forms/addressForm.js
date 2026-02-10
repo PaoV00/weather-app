71cd33dd8599b7985577b88025df2c55c0b037ee
paovn <vangg.ppao@gmail.com>
2026-02-09 20:48:47 -0600

    attempting address form

--- src/components/forms/addressForm.js
+++ src/components/forms/addressForm.js
@@ -1,15 +1,132 @@
 import { useState } from "react";
 import { Form, InputGroup, Button, Row, Col } from "react-bootstrap";
 
-function addressForm() {
-    const [title, setTitle] = useState("");
-    const [condition, setCondition] = useState("");
-    const [currTemp, setCurrTemp] = useState("");
-    const [hiTemp, setHiTemp] = useState("");
-    const [lowTemp, setLowTemp] = useState("");
-    const [cloudCoverage, setCloudCoverage] = useState("");
-    const [wind, setWind] = useState("");
-    const [precipitation, setPrecipitation] = useState("");
+function AddressForm() {
+  const [number, setNumber] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [zip, setZip] = useState("");
+  const [stateCode, setStateCode] = useState("");
+
+  const states = [
+    "AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+  ];
+
+  return (
+    <>
+      <Form>
+        <Form.Label>House Number</Form.Label>
+        <div className="mt-2">
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="House number"
+              value={number}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </InputGroup>
+        </div>
+        <div className="mt-2 mb-2">
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="Street"
+              value={street}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </InputGroup>
+        </div>
+        <div className="mt-2 mb-2">
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="city"
+              value={city}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </InputGroup>
+        </div>
+        <div className="mt-2 mb-2">
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="zip"
+              value={zip}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </InputGroup>
+        </div>
+        <div className="mt-2 mb-2">
+          <Form.Select
+              aria-label="Select State"
+              value={stateCode}
+              required
+              onChange={(e) => setStateCode(e.target.value)}
+            >
+              <option value="">Select State</option>
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </Form.Select>
+        </div>
+      </Form>
+    </>
+  );
 }
 
-export default addressForm;
+export default AddressForm;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,42 @@
+import { Card, Button } from "react-bootstrap";
+import { apiFetch } from "../components/auth/apiFetch";
+import { useSelector } from "react-redux";
+import AddressForm from "../components/forms/addressForm";
 
-function Profile(){
+function Profile() {
 
-    return (
-        <div>
-            <h1>Profile page</h1>
-        </div>
-    );
+  const user = useSelector((state) => state.user.user);
+
+  const handleSetAddress = async (payload) => {
+    try {
+      const userId = user.userId;
+      await apiFetch(`/api/user/${userId}/address`, {
+        method: "PATCH",
+        body: JSON.stringify({ payload }),
+      });
+    } catch (error) {
+      console.error("Error updating address:", error);
+    }
+  };
+
+  return (
+    <div>
+      <Card style={{ width: "28rem" }} className="shadow">
+        <Card.Body>
+          <Card.Title className="text-center mb-4">Address</Card.Title>
+          <AddressForm />
+          <Button
+            variant="primary"
+            className="w-100 mb-3"
+            onClick={() => ""}
+          >
+            Set Address
+          </Button>
+          
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Profile;
